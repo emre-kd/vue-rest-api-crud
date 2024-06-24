1d9ec935702fd6526059de8fda3cd1ed8ad5031f
unknown <emrekaradereli0@gmail.com>
2024-06-24 04:36:45 +0300

    first commit

---
+++ src/views/skills/SkillIndex.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+
+<script setup>
+// Imports
+
+// Uses
+
+// Refs
+
+// Props & Emit
+
+// Computed
+
+// Methods
+
+// Hooks
+
+</script>
+
+<style scoped>
+  
+</style>
